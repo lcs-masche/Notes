b146487f20f435330feea1f095b32cbbb18a0c3c
Moritz Asche <moritz@asches.de>
2021-01-08 12:35:55 -0500

    practicing some coding

---
+++ NumberSystems.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+//find the digits that represent 27 in binary
+
+//use the remainder operator to get the first binary digit
+
+//the remainder operator in %
+27%2
+
+//find the decimal value left to convert
+
+//the division operator is /
+27/2
+
+// get next binary digit
+13%2
+//Get the value left to convert
+13/2
+
+//get next binary digit
+6%2
+//get next remainder value
+6/2
+
+//get next binary digit
+3%2
+//get next remainder value
+3/2
+
+//last binary digit
+1%2
+//Done- because decimal value is zero
+1/2
+
+
+
+206/2
+
